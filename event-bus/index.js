0e449ac00e21706222a5c7df870485e3d76a56af
Rifky Sultan Karisma Andriansyah <batuhc105@gmail.com>
2022-02-03 00:18:28 +0700

    finish mini microservices:

--- event-bus/index.js
+++ event-bus/index.js
@@ -14,16 +14,16 @@ app.post('/events', (req, res) => {
 
     events.push(event);
 
-    axios.post('http://localhost:4000/events', event).catch((error) => {
+    axios.post('http://posts-clusterip-srv:4000/events', event).catch((error) => {
         console.log(error.message);
     });
-    axios.post('http://localhost:4001/events', event).catch((error) => {
+    axios.post('http://comments-clusterip-srv:4001/events', event).catch((error) => {
         console.log(error.message);
     });
-    axios.post('http://localhost:4002/events', event).catch((error) => {
+    axios.post('http://query-clusterip-srv:4002/events', event).catch((error) => {
         console.log(error.message);
     });
-    axios.post('http://localhost:4003/events', event).catch((error) => {
+    axios.post('http://moderation-clusterip-srv:4003/events', event).catch((error) => {
         console.log(error.message);
     });
 
